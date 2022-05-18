67388cfa7b90b60d31462014c658286a8f412866
Zannat <zannatulmawa62@gmail.com>
2022-05-18 12:38:11 -0400

    adding html and bootstrap

--- src/main/java/my/workbench/StudentManagementSystem/controller/StduentController.java
+++ src/main/java/my/workbench/StudentManagementSystem/controller/StduentController.java
@@ -1,6 +1,8 @@
 package my.workbench.StudentManagementSystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import my.workbench.StudentManagementSystem.service.StudentService;
 
@@ -18,7 +20,14 @@ public class StduentController {
 	
 	//creating handler method to handle list students and return mode and view
 	
-	
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		//model is just like HashMap container, it will take two arguments: key and values
+		model.addAttribute("students", studentService.getAllStudents()); 
+		
+		return "students"; //students is the view
+		
+	}
 
 	
 
